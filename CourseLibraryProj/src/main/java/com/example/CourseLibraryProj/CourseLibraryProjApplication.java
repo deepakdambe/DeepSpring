4d055733e53d49ee58676da7ae1b4798d2c3c1f0
Deepak Dambe <deepakdambe@outlook.com>
2023-12-02 16:49:22 +0530

    Initial commit of library project.

---
+++ CourseLibraryProj/src/main/java/com/example/CourseLibraryProj/CourseLibraryProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseLibraryProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseLibraryProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseLibraryProjApplication.class, args);
+	}
+
+}
---
+++ CourseLibraryProj/src/test/java/com/example/CourseLibraryProj/CourseLibraryProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseLibraryProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseLibraryProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
