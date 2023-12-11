5beb3c0dae3cfc507e6c260b6cf83937f41d4245
Deepak Dambe <deepakdambe@outlook.com>
2023-12-11 11:39:25 +0530

    SocialYoutube Initial commit.

---
+++ SocialProjectYoutube/src/main/java/com/example/SocialProjectYoutube/SocialProjectYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SocialProjectYoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialProjectYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialProjectYoutubeApplication.class, args);
+	}
+
+}
---
+++ SocialProjectYoutube/src/test/java/com/example/SocialProjectYoutube/SocialProjectYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialProjectYoutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialProjectYoutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
